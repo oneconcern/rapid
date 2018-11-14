726d3753c9180d5b86c73019929f91441e0dc810
kjfries <kfries@oneconcern.com>
2018-11-14 12:01:39 -0800

    change print

--- src/rapid_read_Qfor_file.F90
+++ src/rapid_read_Qfor_file.F90
@@ -67,7 +67,7 @@ end if
 !*******************************************************************************
 call VecAssemblyBegin(ZV_Qfor,ierr)
 call VecAssemblyEnd(ZV_Qfor,ierr)
-call VecView(IV_for_loc2,PETSC_VIEWER_STDOUT_WORLD,ierr)
+print *, IV_for_loc2
 call VecGetValues(ZV_Qfor,one,IV_for_loc2,out_value,ierr)
 call VecView(out_value,PETSC_VIEWER_STDOUT_WORLD,ierr)
 
